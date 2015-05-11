0409f4c725b67caa3afb37fa480bbf7f60050c58
Dmitry Bushev <bushevdv@gmail.com>
2015-05-11 11:14:08 +0300

    emacs: customize scala-mode2

--- emacs.d/sbt-mode.el
+++ emacs.d/sbt-mode.el
@@ -2,3 +2,13 @@
 
 (prelude-require-package 'sbt-mode)
 
+(add-hook 'scala-mode-hook '(lambda ()
+                              ;; sbt-find-definitions is a command that tries to find (with grep)
+                              ;; the definition of the thing at point.
+                              (local-set-key (kbd "M-.") 'sbt-find-definitions)
+
+                              ;; use sbt-run-previous-command to re-compile your code after changes
+                              (local-set-key (kbd "C-x '") 'sbt-run-previous-command)
+
+                              ;; issue sbt command
+                              (local-set-key (kbd "C-c C-c") 'sbt-command)))
