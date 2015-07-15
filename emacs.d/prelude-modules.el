dd7db3be93d6c3bb4321338f18f728086b4ee420
Dmitry Bushev <bushevdv@gmail.com>
2015-07-15 10:52:00 +0300

    emacs: edit prelude-modules
    prelude-modules now follow the structure of original sample file,
    it allows to track differences between them more easily

--- emacs.d/prelude-modules.el
+++ emacs.d/prelude-modules.el
@@ -1,12 +1,10 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
-(require 'prelude-company)
 (require 'prelude-erc) ;; Emacs IRC client
+(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
 (require 'prelude-helm) ;; Interface for narrowing and search
 (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
-(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
-(require 'prelude-programming) ;; Some basic prog-mode configuration and programming related utilities
+(require 'prelude-company)
 
 ;;; Programming languages support
 (require 'prelude-c)
@@ -18,6 +16,7 @@
 (require 'prelude-latex)
 (require 'prelude-lisp)
 (require 'prelude-ocaml)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-python)
 (require 'prelude-scala)
 (require 'prelude-shell)
