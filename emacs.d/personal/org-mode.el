07611d6b30cde2d2e0bb27b0174c49d3e8100464
Dmitry Bushev <bushevdv@gmail.com>
2015-10-08 15:48:50 +0300

    emacs: org-mode xelatex support

--- emacs.d/personal/org-mode.el
+++ emacs.d/personal/org-mode.el
@@ -1,2 +1,20 @@
+(require 'ox-latex)
+(setq org-latex-listings t)
+
+;;; XeLaTeX customisations
+;; remove "inputenc" from default packages as it clashes with xelatex
+(setf org-latex-default-packages-alist
+      (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
+
+(add-to-list 'org-latex-packages-alist '("" "xltxtra" t))
+;; choose Linux Libertine O as serif and Linux Biolinum O as sans-serif fonts
+(add-to-list 'org-latex-packages-alist '("" "libertine" t))
+
+;; org to latex customisations, -shell-escape needed for minted
+(setq org-latex-pdf-process               ; for regular export
+      '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
 ;; in order to generate bibliography properly we need texi2dvi
-(setq org-latex-pdf-process '("texi2dvi --pdf --clean --verbose --batch %f"))
+;;(setq org-latex-pdf-process '("texi2dvi --pdf --clean --verbose --batch %f"))
