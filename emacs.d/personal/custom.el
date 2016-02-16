d5fa34a8807c8a688d29834e23b27316829460ca
Dmitry Bushev <bushevdv@gmail.com>
2016-02-16 16:33:24 +0300

    emacs: general customizations

---
+++ emacs.d/personal/custom.el
@@ -0,0 +1,25 @@
+;; custom settings
+
+(prelude-require-packages '(transpose-frame))
+
+(require 'transpose-frame)
+
+;; whitespace-mode settings
+;; * do not highlight tabs and mark them as `>>` character
+;;   face tab removed and tab-mark added
+;; * mark newline as `$` character
+;;   face newline-mark
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-style
+   (quote
+    (face newline empty trailing lines-tail tab-mark newline-mark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
