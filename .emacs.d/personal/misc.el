c42c31ac8a4c1a2715f3b14f91618caa02e69af6
Dmitry Bushev <bushevdv@gmail.com>
2016-12-02 13:27:17 +0300

    emacs: helm uses ripgrep for search

--- .emacs.d/personal/misc.el
+++ .emacs.d/personal/misc.el
@@ -1,12 +1,18 @@
 ;; custom settings
 
-(prelude-require-packages '(transpose-frame))
+(prelude-require-packages
+  '(projectile-ripgrep transpose-frame))
 
 (require 'transpose-frame)
+(require 'projectile-ripgrep)
 
 (add-hook 'java-mode-hook
           (lambda ()
             (setq c-basic-offset 2)))
+
+;; helm interactive search with ripgrep
+'(helm-grep-ag-command
+   "TERM=eterm-color rg --color=always --smart-case --no-heading --line-number %s %s %s")
 
 ;; whitespace-mode settings
 ;; * do not highlight tabs and mark them as `>>` character
