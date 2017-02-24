93cf76a50c04a6f1141b00968097754e8845b7a2
Dmitry Bushev <bushevdv@gmail.com>
2017-02-24 14:32:35 +0300

    zsh: remove unused aliases

--- .zshrc.d/90-alias.sh
+++ .zshrc.d/90-alias.sh
@@ -1,12 +1,3 @@
-# sudo
-alias sudo='sudo '
-alias reboot='sudo reboot'
-alias poweroff='sudo poweroff'
-alias wifi-menu='sudo wifi-menu'
-alias tlp='sudo tlp'
-alias wifi='sudo wifi'
-alias bluetooth='sudo bluetooth'
-
 # util
 alias ls='ls -h --color'
 alias ll='ls -l'
@@ -14,20 +5,3 @@ alias la='ls -la'
 alias -g grep='grep --color'
 
 compdef ll=ls la=ls
-
-# docker
-
-docker() {
-  case "$@" in
-    inspect* ) shift; docker_inspect "$@" ;;
-    * ) command docker "$@" ;;
-  esac
-}
-
-docker_inspect() {
-  case "$@" in
-    ip* ) shift; command docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@" ;;
-    pid* ) shift; command docker inspect --format '{{ .State.Pid }}' "$@" ;;
-    * ) command docker inspect "$@" ;;
-  esac
-}
