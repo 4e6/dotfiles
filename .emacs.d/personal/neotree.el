f11b9722beecb6858cd3c4eddb635fc8a558a041
Dmitry Bushev <bushevdv@gmail.com>
2017-04-28 12:32:06 +0300

    emacs: neotree open directory of a buffer

--- .emacs.d/personal/neotree.el
+++ .emacs.d/personal/neotree.el
@@ -1,3 +1,7 @@
 (prelude-require-package 'neotree)
 
-(global-set-key (kbd "C-c p n") 'neotree-toggle)
+(global-set-key
+  (kbd "C-c p n")
+  (lambda ()
+    (interactive)
+    (neotree-dir default-directory)))
