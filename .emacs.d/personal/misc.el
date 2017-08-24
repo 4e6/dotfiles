d75725ae94f8c57981888510be9889daa1f4fcad
Dmitry Bushev <bushevdv@gmail.com>
2017-08-24 15:05:02 +0300

    emacs: sgml set offset 4

--- .emacs.d/personal/misc.el
+++ .emacs.d/personal/misc.el
@@ -49,6 +49,7 @@ reverses order."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(sgml-basic-offset 4)
  '(whitespace-style
    (quote
     (face newline empty trailing lines-tail tab-mark newline-mark))))
