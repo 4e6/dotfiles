0bc283056ed6089e46a59d1214289048dfabb49f
Dmitry Bushev <bushevdv@gmail.com>
2017-10-16 11:42:18 +0300

    emacs: remove agda-mode

--- .emacs.d/personal/agda-mode.el
+++
@@ -1,5 +0,0 @@
-;; agda-mode
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
-;; add stdlib to path
-(setq agda2-include-dirs '("." "/usr/share/agda/lib/prim"))
