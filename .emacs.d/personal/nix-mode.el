0c806231a9cc6fd4243798f5473a6538201f050f
Dmitry Bushev <bushevdv@gmail.com>
2018-08-09 07:22:06 +0300

    emacs: update nix-mode config

--- .emacs.d/personal/nix-mode.el
+++ .emacs.d/personal/nix-mode.el
@@ -1,1 +1,13 @@
 (prelude-require-package 'nix-mode)
+
+(use-package nix-mode
+  :mode ("\\.nix\\'" "\\.nix.in\\'"))
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
