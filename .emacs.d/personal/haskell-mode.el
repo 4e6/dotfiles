70b4baa622c3f5d36006b972ca88dc11d6e62192
Dmitry Bushev <bushevdv@gmail.com>
2019-09-09 11:30:23 +0300

    emacs: haskell-stylish-on-save true

--- .emacs.d/personal/haskell-mode.el
+++ .emacs.d/personal/haskell-mode.el
@@ -1,11 +1,15 @@
 ;; prelude-haskell mode customization
 
-(require 'flycheck)
+(prelude-require-package 'flycheck-haskell)
 (setq flycheck-ghc-args '("-O0"))
 
 (require 'haskell-mode)
+(add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
+
 (defun override-flycheck-haskell-default-directory (proc &rest args)
     (funcall proc 'haskell-ghc))
 (advice-add 'flycheck-haskell--find-default-directory
             :around
             #'override-flycheck-haskell-default-directory)
+(custom-set-variables
+ '(haskell-stylish-on-save t))
