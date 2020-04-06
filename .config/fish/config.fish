e9c6be332b87e8e4d0f9966a46e6d69b58a7093c
Dmitry Bushev <bushevdv@gmail.com>
2020-04-06 15:16:51 +0300

    conf: add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+# Start X at login
+if status is-interactive
+  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+    exec startx -- -keeptty
+  end
+end
+
+# Install fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
