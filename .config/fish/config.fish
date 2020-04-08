4408decaccef3476b6a341cc6a0de78962352716
Dmitry Bushev <bushevdv@gmail.com>
2020-04-08 04:08:59 +0300

    conf: fish: remove fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,18 +5,12 @@ if status is-interactive
   end
 end
 
-# Install fisher
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 # Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
+# run `base16-zenburn` shell command once to set the colors
+#if status --is-interactive
+    #set BASE16_SHELL "$HOME/.config/base16-shell/"
+    #source "$BASE16_SHELL/profile_helper.fish"
+#end
 
 # disable greeting
 set -U fish_greeting
